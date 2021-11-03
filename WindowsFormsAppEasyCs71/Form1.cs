97fe146fde599eed3b2fa8fed8d41fdf26c84982
hideru <jota.16.stigma.world@gmail.com>
2021-11-04 06:59:26 +0900

    test succeeded

--- WindowsFormsAppEasyCs71/Form1.cs
+++ WindowsFormsAppEasyCs71/Form1.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsAppEasyCs71
     {
         private Button[] _buttons = new Button[6];
         private FlowLayoutPanel flp;
+        private TableLayoutPanel tlp;
 
         public void Display()
         {
@@ -28,17 +29,39 @@ namespace WindowsFormsAppEasyCs71
             this.Width = 600;
             this.Height = 100;
 
-            flp = new FlowLayoutPanel();
-            flp.Dock = DockStyle.Fill;
-
-            for (int i = 0; i < _buttons.Length; i++)
-            {
-                _buttons[i] = new Button();
-                _buttons[i].Text = Convert.ToString(i);
-                _buttons[i].Parent = flp;
-            }
+            // flp = new FlowLayoutPanel();
+            // flp.Dock = DockStyle.Fill;
+            // flp.Dock = DockStyle.Bottom;
+            // flp.Dock = DockStyle.Top;
+            // flp.Dock = DockStyle.Left;
+            // flp.Dock = DockStyle.Right;
+            // flp.Anchor = AnchorStyles.None; // Center
             
-            flp.Parent = this;
+            // for (int i = 0; i < _buttons.Length; i++)
+            // {
+            //     _buttons[i] = new Button();
+            //     _buttons[i].Text = Convert.ToString(i);
+            //     _buttons[i].Parent = flp;
+            // }
+            //
+            // flp.Parent = this;
+
+            tlp = new TableLayoutPanel();
+            tlp.Dock = DockStyle.Fill;
+            tlp.ColumnCount = 3;
+            tlp.RowCount = 3;
+            // tlp.SetColumnSpan(_buttons[0], 2); // Unite (Failed)
+            // tlp.SetRowSpan(_buttons[3], 2);
+
+            for (int j = 0; j < _buttons.Length; j++)
+            {
+                _buttons[j] = new Button();
+                if(j == 0) { tlp.SetColumnSpan(_buttons[j], 2); } // Unite (Succeeded)
+                _buttons[j].Text = Convert.ToString(j);
+                _buttons[j].Parent = tlp;
+            }
+
+            tlp.Parent = this;
         }
     }
 }
