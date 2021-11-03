2e820934cdca5a814e6a8718a2f3c4c42539298c
hideru <jota.16.stigma.world@gmail.com>
2021-11-04 06:41:22 +0900

    Buttons appeared

---
+++ WindowsFormsAppEasyCs71/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEasyCs71
+{
+    public partial class Form1 : Form
+    {
+        private Button[] _buttons = new Button[6];
+        private FlowLayoutPanel flp;
+
+        public void Display()
+        {
+            Application.Run(new Form1());
+        }
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Flow Layout Panel Sample";
+            this.Width = 600;
+            this.Height = 100;
+
+            flp = new FlowLayoutPanel();
+            flp.Dock = DockStyle.Fill;
+
+            for (int i = 0; i < _buttons.Length; i++)
+            {
+                _buttons[i] = new Button();
+                _buttons[i].Text = Convert.ToString(i);
+                _buttons[i].Parent = flp;
+            }
+            
+            flp.Parent = this;
+        }
+    }
+}
---
+++ WindowsFormsAppEasyCs71/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEasyCs71
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            // Application.Run(new Form1());
+            Form1 form1 = new Form1();
+            form1.Display();
+        }
+    }
+}
